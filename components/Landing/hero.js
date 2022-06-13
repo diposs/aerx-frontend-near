ea6c1a72f2f73976810a0723c3cdba9e0c1edd90
diposs <onakunle.dipo@gmail.com>
2022-06-13 14:33:23 +0100

    Update hero.js

--- components/Landing/hero.js
+++ components/Landing/hero.js
@@ -11,7 +11,7 @@ import useTranslation from "next-translate/useTranslation";
 
 function HeroSection() {
     const { t } = useTranslation("landing");
-    const { colorMode } = light;
+    const { colorMode } = "light";
     return (
         <Box as="section" pt={100}>
             <Box position="relative">
