1f102e2d1d12521a7a89e0733b0c6d6a51b7216c
diposs <onakunle.dipo@gmail.com>
2022-06-14 22:27:46 +0100

    Update chargeModal.js

--- components/Post/chargeModal.js
+++ components/Post/chargeModal.js
@@ -101,33 +101,8 @@ const ChargeModal = ({ nft, state }) => {
         //.then(() => setCharge(nft.tokenId, newAmount));
         onClose();
     }
-    export async function clickchargePost() {
-        if (nearState?.aexBalance == 0){
-            return;
-        } else {
-        const amount = 1;
-        nearState.tokenContract
-            .ft_transfer(
-                {
-                    receiver_id: nft.owner_id,
-                    amount: amount.toString(),
-                    memo:
-                        "Charge :zap: from " +
-                        nearState?.accountId +
-                        " for your AEXpost id." +
-                        nft.token_id,
-                },
-                "300000000000000", // attached GAS (optional)
-                1, // attached deposit in yoctoNEAR (optional)
-            )
-            .catch((e) => {
-                console.log("Charge failed!", e);
-                console.log("nft.owner_id", nft.owner_id);
-                toast("error", "Charge failed!", "ChargeIderr");
-            });
-        }
-    }
-
+    
+    
 
     return (
         <Modal
