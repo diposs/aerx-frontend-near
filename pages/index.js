58101f37eba6f213666adb1e2423b08525e21183
diposs <onakunle.dipo@gmail.com>
2022-06-15 23:14:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 import Channel from "../components/Landing/channel";
 import Footer from "../components/Footer";
-import { Box, LightMode } from "@chakra-ui/react";
+import { Box, LightMode, ColorModeScript, colorMode } from "@chakra-ui/react";
 
 export default function Home() {
     return (
+        <ColorModeScript initialColorMode={colorMode === 'light' ? 'Light' : 'Light'} />
         <LightMode>
         <Layout>
 
