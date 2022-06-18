5105cb11c7cff63df60e4638d2d7e2b5ab6c5115
diposs <onakunle.dipo@gmail.com>
2022-06-18 20:53:23 +0100

    Update profile.js

--- components/ProfileMain/profile.js
+++ components/ProfileMain/profile.js
@@ -31,7 +31,7 @@ const Profile = ({profiledid}) => {
     if (nearState.profile && profileLoaded === false) {
         setProfileLoaded(true);
     }
-        console.log('profiledid',{profiledid});
+        console.log('profiledid',{profiledid}.profiledid);
        // console.log('profiledid2',profiledid());
 
 
