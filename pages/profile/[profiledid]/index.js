bd85a63ea801aa5a926617756e6b84052496445c
diposs <onakunle.dipo@gmail.com>
2022-06-18 19:54:57 +0100

    Update index.js

--- pages/profile/[profiledid]/index.js
+++ pages/profile/[profiledid]/index.js
@@ -4,9 +4,9 @@ import { Box } from "@chakra-ui/react";
 
 
 const ProfilePage = () => {
-  const router = useRouter()
-  const profiledid = router.query
-  console.log('profiledid',profiledid)
+  const router = useRouter();
+  const profiledid = router.query;
+  console.log('profiledid',profiledid);
   
 
     return (
