056279cf2e6c767b50ac0bf82a5b91e4353ec3c3
diposs <onakunle.dipo@gmail.com>
2022-06-19 00:09:51 +0100

    Update index.js

--- components/ProfileMain/SideBar/index.js
+++ components/ProfileMain/SideBar/index.js
@@ -13,49 +13,48 @@ export default function SideBar({  children, bg, state }) {
 	const [isCollapsed, setIsCollapsed] = useState(false);
 	console.log("state",state);
 	console.log("viewed",profiledlooker.profiledid);
-/*	const [currentProfileview, setCurrentProfileview] = useState();
-    useEffect(() => {
-        async function get_current_profileview() {
-          
-            var res = await nearState.pnftContract.profile_by_id({
-                user_id: nearState.accountId,
-                user_to_find_id:{profiledid}.profiledid?.profiledid,
-            });
+	const [currentProfileview, setCurrentProfileview] = useState();
+	useEffect(() => {
+		async function get_current_profileview() {
+			var res = await nearState.pnftContract.profile_by_id({
+                	user_id: nearState.accountId,
+                	user_to_find_id:profiledlooker.profiledid,
+            	});
 
-            setCurrentProfileview(res);
+           	 setCurrentProfileview(res);
             // return res;
             
-        }
-        get_current_profileview();
-    }, [nearState, nearState.accountId, {profiledid}.profiledid, isOpen]);*/
+        	}
+        	get_current_profileview();
+    	}, [nearState, nearState.accountId, nearState.pnftContract, profiledlooker.profiledid]);
+	console.log('currentProfileview', {currentProfileview});
 
-
-    return (
-	<div>
-        <div className="grid grid-cols-12 gap-x-10 mx-10 dippy dippycontent sm:dippy md:griddy">																																																																																																																																		<div
-                className="col-start-1 col-span-3 flex flex-col items-center min-h-full"
-                trigger={null}
-            >    
-            <NFTCard
-                    profile={state?.profile}
-                    balance={state?.aexBalance}
-                    bg={bg}
-                    className="sticky top-20 min-h-max min-w-full"
-                />        
-        </div>																																																					
-        <div className="col-start-4 col-span-6 flex flex-col items-center">
-            <Content className="min-w-full">{children}</Content>
-        </div>
-        <div className="col-start-10 col-span-3 flex flex-col items-center">
-            <Collections
-                collapse={[isCollapsed, setIsCollapsed]}
-                className="min-w-full"
-            />
-        </div>            
-        </div>
-	<div className="items-center md:dippy">
-                <Content className="min-w-full">{children}</Content>
-        </div>
-	</div>
+	return (
+		<div>
+        		<div className="grid grid-cols-12 gap-x-10 mx-10 dippy dippycontent sm:dippy md:griddy">																																																																																																																																		<div
+                		className="col-start-1 col-span-3 flex flex-col items-center min-h-full"
+                		trigger={null}
+            		>    
+            		<NFTCard
+                    		profile={state?.profile}
+                    		balance={state?.aexBalance}
+                   		 bg={bg}
+                    		className="sticky top-20 min-h-max min-w-full"
+                	/>        
+       			</div>																																																					
+        		<div className="col-start-4 col-span-6 flex flex-col items-center">
+            			<Content className="min-w-full">{children}</Content>
+        		</div>
+        		<div className="col-start-10 col-span-3 flex flex-col items-center">
+            			<Collections
+               		 		collapse={[isCollapsed, setIsCollapsed]}
+                			className="min-w-full"
+            			/>
+        		</div>            
+        	</div>
+		<div className="items-center md:dippy">
+                	<Content className="min-w-full">{children}</Content>
+        	</div>
+		</div>
     );
 }
