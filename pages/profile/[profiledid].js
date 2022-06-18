7eb7f9b8a2542bb867cdd696859b527c1c7aa882
diposs <onakunle.dipo@gmail.com>
2022-06-18 18:17:20 +0100

    Update [profiledid].js

--- pages/profile/[profiledid].js
+++ pages/profile/[profiledid].js
@@ -1,10 +1,29 @@
 import { nearStore } from "../../stores/near.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Profile from "../../components/ProfileMain";
 import { Box } from "@chakra-ui/react";
 
 export async function getServerSideProps({ params }) {
   const profiledid = params.profiledid;
+  const nearState = nearStore((state) => state);
+  const [profiledidView, setProfiledidView] = useState();
+    useEffect(() => {
+        async function get_current_profile() {
+            if( profiledid ===  "Aerx.testnet" ){
+                return
+            } else {
+            var res = await nearState.pnftContract.profile_by_id({
+                user_id: nearState.accountId,
+                user_to_find_id: profiledid,
+            });
+
+            setProfiledidView(res);
+            // return res;
+            }
+        }
+        get_current_profile();
+    }, [nearState, nearState.pnftContract, nearState.accountId, profiledid, ]);
+  const profiledidViewer = await.get_current_profile().then(res => res.json());
   return {
     props: {
       profiledid,
