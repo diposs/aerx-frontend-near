62bb7866cd0404408588fbc8545fcb13166e7932
diposs <onakunle.dipo@gmail.com>
2022-06-18 12:32:46 +0100

    Update [profiledid].js

--- pages/profile/[profiledid].js
+++ pages/profile/[profiledid].js
@@ -2,16 +2,6 @@ import { nearStore } from "../../stores/near.js";
 import { useState } from "react";
 import Profile from "../../components/Profile";
 import { Box } from "@chakra-ui/react";
-const nearState = nearStore((state) => state);
-    async function get_current_profile() {
-
-  const res = await nearState.pnftContract.profile_by_id({
-                user_id: nearState.accountId,
-                user_to_find_id: 'dipo1.testnet',
-            });
-  const data = await res.json();
-  };
-  console.log("dd",data);
 const ProfilePage = () => {
     return (
         <Box>
@@ -19,10 +9,5 @@ const ProfilePage = () => {
         </Box>
     );
 };
-export async function getServerSideProps() {
-  // Fetch data from external API
-  
-  // Pass data to the page via props
-  return { props:{ message: `Next.js is awesome` } }
-}
+
 export default ProfilePage;
