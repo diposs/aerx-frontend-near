9f0bb3590d16066233530c6c41d87e0c9692dd8c
diposs <onakunle.dipo@gmail.com>
2023-02-24 19:01:46 +0100

    Delete components directory

--- components/Hero/PlayButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Circle } from "@chakra-ui/react";
-
-const PlayButton = () => {
-    return <Circle>PlayButton</Circle>;
-};
-
-export default PlayButton;
--- components/Landing/index.js
+++
@@ -1,6 +0,0 @@
-import HeroSection from "./hero";
-import Features from "./features";
-import Team from "./team";
-import EmailCapture from "./email-capture";
-
-export { HeroSection, EmailCapture, Features, Team };
